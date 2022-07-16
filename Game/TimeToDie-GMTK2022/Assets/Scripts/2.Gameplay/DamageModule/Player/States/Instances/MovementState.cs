7837c91d8bab16a33c07d82ea161f190174618ef
Dardranight7 <dardranight@gmail.com>
2022-07-16 14:02:06 -0500

    Character Animations

--- Game/TimeToDie-GMTK2022/Assets/Scripts/2.Gameplay/DamageModule/Player/States/Instances/MovementState.cs
+++ Game/TimeToDie-GMTK2022/Assets/Scripts/2.Gameplay/DamageModule/Player/States/Instances/MovementState.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class MovementState : PlayerStateBase
 {
-    int verticalHash;
+    int velocityHash;
     int horizontalHash;
     public MovementState(PlayerStateMachine player) : base(player)
     {
         this.player = player;
-        verticalHash = Animator.StringToHash("isMoving");
-        horizontalHash = Animator.StringToHash("isRotating");
+        velocityHash = Animator.StringToHash("Velocity");
+        horizontalHash = Animator.StringToHash("HorizontalVelocity");
     }
 
     public override void ProcessInput(Vector2 movement, Vector3 look)
@@ -18,6 +18,8 @@ public class MovementState : PlayerStateBase
         player.Indicator.transform.position = look;
         player.RigidBody.velocity = (player.transform.forward * movement.y + player.transform.right * movement.x).normalized * player.Stats.Velocity * 100 * Time.deltaTime;
         player.transform.LookAt(new Vector3(look.x,player.transform.position.y,look.z));
+        player.animator.SetFloat(velocityHash, movement.y);
+        player.animator.SetFloat(horizontalHash, movement.x);
     }
 
     public override void ReceivedEvent(PlayerStateMachine.Buttons buttons)
